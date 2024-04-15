3e4464b5046cd23633fa606c4402d5be5193961e
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-15 05:05:09 -0300

    Feat: creating route from GET method

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3333;
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
 
+const PORT = 3333;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
