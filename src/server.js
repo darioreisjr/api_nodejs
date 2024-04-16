1fef72a7d7d083658f6cb97e445bbee3025cde40
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-16 04:34:26 -0300

    Feat: Implementing Route Params in the application

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(
+    `Id da mensagem: ${id}.
+    Para o usuário: ${user}.
+    `);
 });
 
 const PORT = 3333;
