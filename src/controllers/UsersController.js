343841e7eb0f80d73e6d9007c056d35405f9f97d
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-17 05:12:16 -0300

    feat: Creating our application's controller folder

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+class UserController {
+/**
+ * index - GET para listar vários registros
+ * show - GET para mostrar um registro
+ * create - POST para criar um registro
+ * update - PUT para atualizar um registro
+ * delete - DELETE para remover um registro
+ */
+
+}
+
+module.exports = UserController;
