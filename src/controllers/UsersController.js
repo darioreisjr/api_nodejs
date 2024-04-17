17f7d0e558d3d89f95ff3dd074fd3c077e5b4be5
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-17 05:41:04 -0300

    Feat: Passing the ststus code to our UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,8 @@
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body;
-    response.json({ name, email, password });
+
+    response.status(201).json({ name, email, password });
   }
 }
 
