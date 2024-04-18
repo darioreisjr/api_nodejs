0b78d5ad305250307d97f006a0370b5b1fd52996
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-18 05:36:59 -0300

    feat: Handling exceptions within the application

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError');
+
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if (!name) {
+      throw new AppError('Name is required');
+    }
 
     response.status(201).json({ name, email, password });
   }
