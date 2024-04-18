20dac7b5496ec2b2423ac40dc48027f896e8409f
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-18 04:48:09 -0300

    Feat: Implementing and testing middleware example

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,23 @@ const UserController = require('../controllers/UsersController');
 
 const usersRoutes = Router();
 
+function myMidddleware(request, response, next) {
+  console.log('Você passou pelo midddleware');
+
+  if(!request.body.isAdmin){
+    return response.json({ message: 'User is not admin' });
+  }
+
+  next(); // para a execução do middleware em seguida
+}
+
+
+
+
+
+
 const usersController = new UserController();
 
-usersRoutes.post('/', usersController.create);
+usersRoutes.post('/', myMidddleware, usersController.create);
 
 module.exports = usersRoutes;
