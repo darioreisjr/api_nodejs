23b3a60e76e8e0aacaebafa688517c92e6976fb1
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-22 04:37:29 -0300

    Feat: Building our application's select and connection to our database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,19 @@
 const AppError = require('../utils/AppError');
 
+const sqliteConnection = require('../database/sqlite');
+
 class UserController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError('Name is required');
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail ja existe");
     }
 
-    response.status(201).json({ name, email, password });
+    return response.status(201).json();
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,23 +4,8 @@ const UserController = require('../controllers/UsersController');
 
 const usersRoutes = Router();
 
-function myMidddleware(request, response, next) {
-  console.log('Você passou pelo midddleware');
-
-  if(!request.body.isAdmin){
-    return response.json({ message: 'User is not admin' });
-  }
-
-  next(); // para a execução do middleware em seguida
-}
-
-
-
-
-
-
 const usersController = new UserController();
 
-usersRoutes.post('/', myMidddleware, usersController.create);
+usersRoutes.post('/', usersController.create);
 
 module.exports = usersRoutes;
