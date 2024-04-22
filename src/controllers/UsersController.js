655cbcb0d1e491b6841d4e0a50bbf8fe781d26c7
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-22 05:36:30 -0300

    Feat: Creating routes for updating users, blocking errors and authenticating if there are duplicate emails

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,6 +22,38 @@ class UserController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const {name, email} = request.body;
+    const {id} = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail ja existe");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date(), id]
+    );
+
+    return response.json();
+  }
 }
 
 module.exports = UserController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,4 +8,6 @@ const usersController = new UserController();
 
 usersRoutes.post('/', usersController.create);
 
+usersRoutes.put('/:id', usersController.update);
+
 module.exports = usersRoutes;
