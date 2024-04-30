fa46b29426b1a2fc427e651b39176fee8ef4fcdb
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-30 05:10:15 -0300

    Feat: Validating registered user if it exists in our database

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,17 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+
 class SessionController {
   async create(request, response) {
     const { email, password } = request.body;
 
-    return response.json({email, password});
+    const user = await knex('users').where({ email }).first();
+
+    if (!user) {
+      throw new AppError('E-mail e/ou senha inválidos', 401);
+    }
+
+    return response.json(user);
   }
 }
 
