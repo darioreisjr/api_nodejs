184147eff9d7828540a1aeda82dae80bfa9c5c4e
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-30 05:37:04 -0300

    Feat: Installing the jwt library, creating jwt configuration file and creating authentication for token return

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
 const { compare } = require('bcryptjs');
+const authConfig = require('../configs/auth');
+const { sign } = require('jsonwebtoken');
 
 class SessionController {
   async create(request, response) {
@@ -18,7 +20,14 @@ class SessionController {
       throw new AppError('E-mail e/ou senha inválidos', 401);
     }
 
-    return response.json(user);
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    });
+
+    return response.json({ user, token });
   }
 }
 
