1ace20879b03e3f5b01231ba8b05348c4359fd1e
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-30 05:00:33 -0300

    Feat: Creating session authentication route in our application

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,9 @@
+class SessionController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    return response.json({email, password});
+  }
+}
+
+module.exports = SessionController;
