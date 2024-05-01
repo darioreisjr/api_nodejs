16e27396ebafa6ac3996a906269b9b250ef1e312
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-01 19:01:16 -0300

    Feat: Configuring image upload and creating upload routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,21 @@
 const { Router } = require('express');
+const multer = require('multer');
+const uploadConfig = require('../configs/upload');
 
 const UserController = require('../controllers/UsersController');
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
+const upload = multer(uploadConfig.MULTER);
+
 const usersController = new UserController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (request, response) => {
+  console.log(request.file.filename);
+  return response.json();
+});
 
 module.exports = usersRoutes;
