a6a98f63cf073c5b777e8c892aecd3b3e4a9ec8d
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-01 19:35:15 -0300

    Feat: Saving image to database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const uploadConfig = require('../configs/upload');
 
 const UserController = require('../controllers/UsersController');
+const UserAvatarController = require('../controllers/UserAvatarController');
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
@@ -10,12 +11,10 @@ const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UserController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (request, response) => {
-  console.log(request.file.filename);
-  return response.json();
-});
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
 module.exports = usersRoutes;
