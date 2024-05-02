0a9826946f73303c8eaf73c21abcd1dda224f497
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-02 04:33:56 -0300

    Feat: Installing and configuring the cors library to integrate with our front-end

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const uploadConfig = require('./configs/upload');
 
+const cors = require('cors');
 const express = require('express');
 
 const routes = require('./routes');
@@ -11,6 +12,7 @@ const routes = require('./routes');
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
